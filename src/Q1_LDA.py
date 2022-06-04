362ccc9aab7a608f6479786cae2d0b3fcca92792
MIracleyin <miracleyin@live.com>
2022-06-04 12:21:51 +0800

    Q1 resolve

--- src/Q1_LDA.py
+++ src/Q1_LDA.py
@@ -2,73 +2,75 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
-from utils import processed_travel, stop_path
+from utils import processed_travel, processed_news, stop_path, resolve_q1
 from utils import get_topic_list, get_stop_words, get_topic_words
 
 if __name__ == '__main__':
     # load processed data
-    travel = pd.read_csv(processed_travel)
-    topic_list = get_topic_list()
-    stop_list = get_stop_words(stop_path)
-    travel_corpus = travel['text'].tolist()
-
-    # word 2 vecter (count tf)
-    n_feature = 250
-    cntVector = CountVectorizer(strip_accents='unicode',
-                                stop_words=stop_list,
-                                max_df=0.5,
-                                min_df=10)
-    cntTf = cntVector.fit_transform(travel_corpus)
-    print(cntTf)
-
-    # build LDA model
-    n_topics = 6
-    lda = LatentDirichletAllocation(n_components=n_topics,
-                                    max_iter=50,
-                                    learning_method='batch',
-                                    learning_offset=50.,
-                                    # doc_topic_prior=0.1,
-                                    # topic_word_prior=0.01,
-                                    random_state=0)
-    docres = lda.fit_transform(cntTf)
-
-    # get each topic words
-    n_top_words = 30
-    tf_feature_names = cntVector.get_feature_names_out()
-    topic_word = get_topic_words(lda, tf_feature_names, n_top_words)
-    print(topic_word)
-    topic_dict = {}
-    for t_id, topic in enumerate(topic_word):
-        t_list = topic.split()
-        t_count = 0
-        for w in t_list:
-            if w in topic_list:
-                t_count += 1
-        topic_dict[t_id] = t_count
-
-    print(topic_dict)
-
-
-    # get each text topic (postprocess)
-    topics = lda.transform(cntTf)
-    topic = []
-    is_related = []
-    for t in topics:
-        topic.append("Topic #"+str(list(t).index(np.max(t))))
-        related_number = topic_dict[list(t).index(np.max(t))]
-        is_related.append(1 if related_number > 0 else 0)
-    travel['is_realted'] = is_related
-    travel_res = pd.DataFrame(travel, columns=['ID', 'is_realted'])
-    travel_res.to_csv("dataset/results/travel_realte.csv", index=0)
-
-
-
-
-    import pyLDAvis
-    import pyLDAvis.sklearn
-
-    pic = pyLDAvis.sklearn.prepare(lda, cntTf, cntVector)
-    pyLDAvis.display(pic)
-    pyLDAvis.save_html(pic, 'lda_pass' + str(8) + '.html')
-    pyLDAvis.display(pic)
+    news = pd.read_csv(processed_news)
+    resolve_q1(news, "news_res.csv", n_topics=8, threshold=0)
+    # topic_list = get_topic_list()
+    # stop_list = get_stop_words(stop_path)
+    # travel_corpus = travel['text'].tolist()
+    #
+    # # word 2 vecter (count tf)
+    # n_feature = 250
+    # cntVector = CountVectorizer(strip_accents='unicode',
+    #                             max_features=n_feature,
+    #                             stop_words=stop_list,
+    #                             max_df=0.5,
+    #                             min_df=10)
+    # cntTf = cntVector.fit_transform(travel_corpus)
+    # print(cntTf)
+    #
+    # # build LDA model
+    # n_topics = 6
+    # lda = LatentDirichletAllocation(n_components=n_topics,
+    #                                 max_iter=50,
+    #                                 learning_method='batch',
+    #                                 learning_offset=50.,
+    #                                 # doc_topic_prior=0.1,
+    #                                 # topic_word_prior=0.01,
+    #                                 random_state=0)
+    # docres = lda.fit_transform(cntTf)
+    #
+    # # get each topic words
+    # n_top_words = 30
+    # tf_feature_names = cntVector.get_feature_names_out()
+    # topic_word = get_topic_words(lda, tf_feature_names, n_top_words)
+    # print(topic_word)
+    # topic_dict = {}
+    # for t_id, topic in enumerate(topic_word):
+    #     t_list = topic.split()
+    #     t_count = 0
+    #     for w in t_list:
+    #         if w in topic_list:
+    #             t_count += 1
+    #     topic_dict[t_id] = t_count
+    #
+    # print(topic_dict)
+    #
+    #
+    # # get each text topic (postprocess)
+    # topics = lda.transform(cntTf)
+    # topic = []
+    # is_related = []
+    # for t in topics:
+    #     topic.append("Topic #"+str(list(t).index(np.max(t))))
+    #     related_number = topic_dict[list(t).index(np.max(t))]
+    #     is_related.append(1 if related_number > 0 else 0)
+    # travel['is_realted'] = is_related
+    # travel_res = pd.DataFrame(travel, columns=['ID', 'is_realted'])
+    # travel_res.to_csv("dataset/results/travel_realte.csv", index=0)
+    #
+    #
+    #
+    #
+    # import pyLDAvis
+    # import pyLDAvis.sklearn
+    #
+    # pic = pyLDAvis.sklearn.prepare(lda, cntTf, cntVector)
+    # pyLDAvis.display(pic)
+    # pyLDAvis.save_html(pic, 'lda_pass' + str(8) + '.html')
+    # pyLDAvis.display(pic)
 
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,21 @@
+import os.path
 from typing import Optional
 import jieba
 import re
+
+import pandas as pd
+import numpy as np
+import pyLDAvis
+import pyLDAvis.sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 raw_part1_path = "dataset/dataset-2018-2019.xlsx"
 raw_part2_path = "dataset/dataset-2020-2021.xlsx"
 processed_travel = "dataset/processed/travelid_text.csv"
 processed_news = "dataset/processed/newsid_text.csv"
 stop_path = "dataset/stopwords.txt"
+
 
 def clean_text(text: str):
     if not isinstance(text, str):
@@ -57,6 +66,7 @@ def get_topic_list():
     topic = "旅游、活动、节庆、特产、交通、酒店、景区、景点、文创、文化、乡村旅游、民宿、假日、假期、游客、采摘、赏花、春游、踏青、康养、公园、滨海游、度假、农家乐、剧本杀、旅行、徒步、工业旅游、线路、自驾游、 团队游、攻略、游记、包车、玻璃栈道、游艇、高尔夫、温泉"
     return topic.split("、")
 
+
 def get_topic_words(model, feature_names, n_top_words):
     tword = []
     for topic_idx, topic in enumerate(model.components_):
@@ -66,3 +76,67 @@ def get_topic_words(model, feature_names, n_top_words):
         print(topic_w)
     return tword
 
+
+def resolve_q1(process_data: pd.DataFrame, save_path: str,
+               is_vis=False,
+               n_topics=5,
+               n_topic_words=30,
+               threshold=0,
+               n_feature=1000,
+               ):
+    assert 'ID' in process_data.columns
+    assert 'text' in process_data.columns
+    process_data = process_data.dropna()
+    # get data
+    corpus = process_data['text'].tolist()
+
+    # transform to vector by cnt
+    cnt_vector = CountVectorizer(strip_accents='unicode',
+                                 # max_features=n_feature,
+                                 stop_words=get_stop_words(stop_path),
+                                 max_df=0.5,
+                                 min_df=10)
+    cnt_tf = cnt_vector.fit_transform(corpus)
+
+    # build LDA model
+    lda = LatentDirichletAllocation(n_components=n_topics,
+                                    max_iter=50,
+                                    learning_method='batch',
+                                    learning_offset=50.,
+                                    # doc_topic_prior=0.1,
+                                    # topic_word_prior=0.01,
+                                    random_state=0)
+    docs_res = lda.fit_transform(cnt_tf)
+
+    # get each topic words
+    tf_feature_names = cnt_vector.get_feature_names_out()
+    topic_word = get_topic_words(lda, tf_feature_names, n_topic_words)
+    print(topic_word)
+
+    # get topic count(appare in topic_list) map
+    topic_list = get_topic_list()
+    topic_count_dict = {}
+    for t_id, topic in enumerate(topic_word):
+        t_list = topic.split()
+        t_count = 0
+        for w in t_list:
+            if w in topic_list:
+                t_count += 1
+        topic_count_dict[t_id] = t_count
+    print(topic_count_dict)
+
+    # get results
+    topics = lda.transform(cnt_tf)
+    is_related = []
+    for t in topics:
+        related_number = topic_count_dict[list(t).index(np.max(t))]
+        is_related.append(1 if related_number > threshold else 0)
+    process_data['is_realted'] = is_related
+    process_data = pd.DataFrame(process_data, columns=['ID', 'is_realted'])
+    process_data.to_csv(os.path.join("dataset/results", save_path), index=0)
+
+    if is_vis:
+        pic = pyLDAvis.sklearn.prepare(lda, cnt_tf, cnt_vector)
+        pyLDAvis.display(pic)
+        pyLDAvis.save_html(pic, 'lda_pass' + str(8) + '.html')
+        pyLDAvis.display(pic)
